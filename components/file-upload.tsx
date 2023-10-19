4b64cd25ece0c470558e1f7424070f404c5ede7d
leobewater <mmbee888@gmail.com>
2023-10-19 10:55:19 -0400

    set up file upload component

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,24 @@
 "use client";
 
-export const FileUpload = () => {
-  return <div>FileUpload</div>;
+import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.error(error);
+      }}
+    />
+  );
 };
