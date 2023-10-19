79210d07d7c4c8497db9d2c957387f865ab9d5b6
leobewater <mmbee888@gmail.com>
2023-10-19 12:32:14 -0400

    avoid hydration error for modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // prevent hydration error
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <CreateServerModal />
