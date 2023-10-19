9fb9e353bed1ce5b6d64c899fa7129f688e984fe
leobewater <mmbee888@gmail.com>
2023-10-19 16:22:18 -0400

    add member role update api endpoint

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,64 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+    const serverId = searchParams.get("serverId");
+    if (!serverId) {
+      return new NextResponse("Missing Server ID", { status: 400 });
+    }
+    if (!params.memberId) {
+      return new NextResponse("Missing Member ID", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              // avoid changing admin users
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBERS_ID_PATH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
