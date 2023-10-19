ff6b9f2d31b73202cbec7cc958c6e83dbb02f7bd
leobewater <mmbee888@gmail.com>
2023-10-19 15:49:23 -0400

    fix types

--- types.ts
+++ types.ts
@@ -2,5 +2,5 @@ import { Member, Profile, Server } from "@prisma/client";
 
 // create custom type extending member and profile
 export type ServerWithMembersWithProfiles = Server & {
-  members: Member & { profile: Profile }[];
+  members: (Member & { profile: Profile })[];
 };
