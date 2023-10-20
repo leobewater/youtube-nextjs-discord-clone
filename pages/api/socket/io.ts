3876074420623509489050a9a1ac897b48cdeaf6
leobewater <mmbee888@gmail.com>
2023-10-20 10:54:02 -0400

    import socket.io

---
+++ pages/api/socket/io.ts
@@ -0,0 +1,4 @@
+// Socket.io not 100% support next.js v13, use old pages folder for now
+import { Server as NetServer } from "http";
+import { NextApiRequest } from "next";
+import { Server as ServerIO } from "socket.io";
