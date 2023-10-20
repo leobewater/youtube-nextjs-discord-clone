b046fa432692f45e155bcb0847dbabb5103295e3
leobewater <mmbee888@gmail.com>
2023-10-20 09:40:32 -0400

    set up chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Hash, Menu } from "lucide-react";
 
-export const ChatHeader = () => {
-  return (
-    <div>ChatHeader</div>
-  )
+interface ChatHeaderProps {
+  serverId: string;
+  name: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
 }
+
+export const ChatHeader = ({
+  serverId,
+  name,
+  type,
+  imageUrl,
+}: ChatHeaderProps) => {
+  return (
+    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <Menu className="" />
+
+      {type === "channel" && (
+        <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 ml-2" />
+      )}
+      <p className="text-black dark:text-white">{name}</p>
+    </div>
+  );
+};
