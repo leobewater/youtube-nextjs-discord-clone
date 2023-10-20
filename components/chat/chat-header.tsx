822f5f4244e5c590d714d2a6381ae213e65fcc0a
leobewater <mmbee888@gmail.com>
2023-10-20 09:46:10 -0400

    set up mobile menu

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -16,10 +16,10 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle />
+      <MobileToggle serverId={serverId} />
 
       {type === "channel" && (
-        <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 ml-2" />
+        <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
       <p className="text-black dark:text-white">{name}</p>
     </div>
