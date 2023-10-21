b033a2e1fd1422d912a072fd66ee0b24cda8994c
leobewater <mmbee888@gmail.com>
2023-10-21 05:42:58 -0400

    copy socker api endpoints

--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -1,11 +1,12 @@
+import { NextApiRequest } from "next";
+
+import { NextApiResponseServerIo } from "@/types";
 import { currentProfilePages } from "@/lib/current-profile-pages";
 import { db } from "@/lib/db";
-import { NextApiResponseServerIo } from "@/types";
-import { NextApiRequest } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponseServerIo
+  res: NextApiResponseServerIo,
 ) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -13,21 +14,23 @@ export default async function handler(
 
   try {
     const profile = await currentProfilePages(req);
-    if (!profile) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
     const { content, fileUrl } = req.body;
     const { serverId, channelId } = req.query;
-
+    
+    if (!profile) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }    
+  
     if (!serverId) {
-      return res.status(400).json({ error: "Missing Server ID" });
+      return res.status(400).json({ error: "Server ID missing" });
     }
+      
     if (!channelId) {
-      return res.status(400).json({ error: "Missing Channel ID" });
+      return res.status(400).json({ error: "Channel ID missing" });
     }
+          
     if (!content) {
-      return res.status(400).json({ error: "Missing Content" });
+      return res.status(400).json({ error: "Content missing" });
     }
 
     const server = await db.server.findFirst({
@@ -35,13 +38,13 @@ export default async function handler(
         id: serverId as string,
         members: {
           some: {
-            profileId: profile.id,
-          },
-        },
+            profileId: profile.id
+          }
+        }
       },
       include: {
         members: true,
-      },
+      }
     });
 
     if (!server) {
@@ -52,17 +55,14 @@ export default async function handler(
       where: {
         id: channelId as string,
         serverId: serverId as string,
-      },
+      }
     });
 
-    if (!server) {
+    if (!channel) {
       return res.status(404).json({ message: "Channel not found" });
     }
 
-    // find current member
-    const member = server.members.find(
-      (member) => member.profileId === profile.id
-    );
+    const member = server.members.find((member) => member.profileId === profile.id);
 
     if (!member) {
       return res.status(404).json({ message: "Member not found" });
@@ -79,18 +79,18 @@ export default async function handler(
         member: {
           include: {
             profile: true,
-          },
-        },
-      },
+          }
+        }
+      }
     });
 
-    // create channelKey for socket.io and dispatch message
-    const channelKey = `chat:${channelId}:messages`
+    const channelKey = `chat:${channelId}:messages`;
+
     res?.socket?.server?.io?.emit(channelKey, message);
 
     return res.status(200).json(message);
   } catch (error) {
-    console.log("[MESSAGES_POSt]", error);
-    return res.status(500).json({ message: "Internal Error" });
+    console.log("[MESSAGES_POST]", error);
+    return res.status(500).json({ message: "Internal Error" }); 
   }
 }
